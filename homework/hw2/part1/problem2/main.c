5501030f31b88ae96c777ba04058c7f7bb840a00
Joseph Moore <joemoore94@gmail.com>
2022-02-06 11:46:14 -0800

    more for p.2

--- homework/hw2/part1/problem2/main.c
+++ homework/hw2/part1/problem2/main.c
@@ -1,6 +1,6 @@
 /* File: main.c
  * Author: Joseh Moore
- * Purpose: test matrix uploader
+ * Purpose: test some basic functions
  */
 
 #include <stdlib.h>
@@ -9,19 +9,24 @@
 #include "../ColMajorMat.h"
 #include "../MatFunc.h"
 
+/*
+Pretty straight forward space separated answers output to the terminal
+*/
+
 int main(int argc, char const *argv[]) {
     ColMajorMat A = CreateMatrix("../Amat.dat");
-    ColMajorMat C = CreateMatrix("../Cmat.dat");
+
+    PrintMat(A, A->m, A->n);
 
     double trace = 0;
     MatTrace(A, A->m, &trace);
-    printf("%f\n", trace);
+    printf("\ntrace(A) = %f\n\n", trace);
 
-    double norm = 0;
-    TwoNorm(C, C->m, &norm);
-    printf("%f\n", norm);
-
-    PrintMat(A, A->m, A->n);
+    for (int i = 0; i < A->n; i++) {
+        double norm = 0;
+        TwoNormCol(A, A->m, i, &norm);
+        printf("||A%d|| = %f\n", i, norm);
+    }
 
     return 0;
 }
