2840e40aac4dfa5a9f0598ec25d2004f2dc71c33
Joseph Moore <joemoore94@gmail.com>
2022-03-13 18:37:26 -0700

    prob b added

---
+++ homework/Final_Project/part1/prob_b/ErrorPlot.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig, ax = plt.subplots()
+
+data = np.loadtxt(f"JacobiData.dat")
+plt.plot(data[:,0],data[:,1], label='Jacobi')
+
+data = np.loadtxt(f"SeidelData.dat")
+plt.plot(data[:,0],data[:,1], label='Seidel')
+
+plt.xlabel("Interations")
+plt.ylabel("Error")
+ax.set_yscale('log')
+ax.legend()
+plt.title("D = 2")
+plt.show()
